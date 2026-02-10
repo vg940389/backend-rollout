ddf55423a5ec31845ceb9fd31e6770f211776cbf
Venkat Golakoti <venkat.g93@gmail.com>
2026-02-10 16:06:37 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/api/hello", () => "Hello, EDP!");
+app.MapGet("/api/hello", () => "Hello, KubeRocketCI from $HOSTNAME");
 
 app.Run();
+
